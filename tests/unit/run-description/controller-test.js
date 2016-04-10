3c2783e3eeb5299958fc8e86bbb47c5ef43c1225
John Whittenburg <jawhittenburg@gmail.com>
2016-04-10 16:30:40 -0500

    update to run-description route

---
+++ tests/unit/run-description/controller-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:run-description', 'Unit | Controller | run description', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
---
+++ tests/unit/run-description/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:run-description', 'Unit | Route | run description', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
